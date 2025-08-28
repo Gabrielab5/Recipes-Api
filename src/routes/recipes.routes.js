77cda42099700a8f9abb690222b8cd8c72eab999
Gabriela <gabriela@gmail.com>
2025-08-28 16:28:59 +0300

    Completed Routes file

--- src/routes/recipes.routes.js
+++ src/routes/recipes.routes.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const { getAllRecipes, createRecipe } = require('../controllers/recipes.controller');
+const { getAllRecipes, getRecipeById, createRecipe, updateRecipe, deleteRecipe, getRecipeStats } = require('../controllers/recipes.controller');
 const { validateRecipe } = require('../middlewares/validationMiddleware');
 
 // Retrieve all recipes
 router.get('/', getAllRecipes);
 
+// Get recipe statistics (should come before /:id to avoid conflicts)
+router.get('/stats', getRecipeStats);
+
+// Retrieve a single recipe by ID
+router.get('/:id', getRecipeById);
+
 // Create a new recipe
 router.post('/', validateRecipe, createRecipe);
+
+// Update an existing recipe
+router.put('/:id', validateRecipe, updateRecipe);
+
+// Delete a recipe
+router.delete('/:id', deleteRecipe);
+
 
 module.exports = router;
